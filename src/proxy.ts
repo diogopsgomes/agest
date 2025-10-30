6e610862d5fa01d7e24351dcf8cb30955eb15258
Diogo Gomes <diogolive@hotmail.com>
2025-10-30 22:34:40 +0000

    nextjs upgrade

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import {
   NextResponse,
-  type MiddlewareConfig,
+  type ProxyConfig,
   type NextRequest,
 } from "next/server";
 
@@ -12,7 +12,7 @@ const publicRoutes = [
 
 const REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE = "/entrar";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const path = request.nextUrl.pathname;
   const publicRoute = publicRoutes.find((route) => route.path === path);
   const authToken = request.cookies.get("token");
@@ -68,6 +68,6 @@ export async function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
-export const config: MiddlewareConfig = {
+export const config: ProxyConfig = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|icon.svg).*)"],
 };
